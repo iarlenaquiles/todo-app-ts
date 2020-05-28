962d989f9978a746dbde6bdf11efa00d97a45bf4
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-05-27 22:18:50 -0300

    create todoform component

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import shortid from 'shortid';
+
+import { ITodo, ITodoForm } from '../interfaces'
+
+const TodoForm = (props: ITodoForm) => {
+  const inputRef = React.useRef<HTMLInputElement>(null)
+
+  const [formState, setFormState] = React.useState('')
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setFormState(event.target.value)
+  }
+
+  function handleInputEnter(event: React.KeyboardEvent) {
+    if (event.key === 'Enter') {
+      const newTodo: ITodo = {
+        id: shortid.generate(),
+        text: formState,
+        isCompleted: false
+      }
+
+      props.handleCreate(newTodo)
+
+      if (inputRef && inputRef.current) {
+        inputRef.current.value = ''
+      }
+    }
+  }
+
+  return (
+    <div className="todo-form">
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder='Enter new todo'
+        onChange={event => handleInputChange(event)}
+        onKeyPress={event => handleInputEnter(event)}
+      />
+    </div>
+  )
+}
+
+export default TodoForm;
