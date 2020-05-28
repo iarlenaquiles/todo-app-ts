9352db8a1b6d607af1aa144a387f665f4edd1af6
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-05-27 22:19:07 -0300

    create todolist component

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import TodoItem from './TodoItem';
+
+import { ITodoList } from '../interfaces'
+
+const TodoList = (props: ITodoList) => {
+  return (
+    <div className="todo-list">
+      <ul>
+        {props.todos.map((todo) => (
+          <li key={todo.id}>
+            <TodoItem
+              todo={todo}
+              handleUpdate={props.handleUpdate}
+              handleRemove={props.handleRemove}
+              handleComplete={props.handleComplete}
+              handleBlur={props.handleBlur}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default TodoList
